2955f464612263014cce5b59b4ba43cd3ce3fef8
sheilkumar <sk17@illinois.edu>
2021-11-08 10:24:56 +0000

    HTML extracted and scraped for wanted attributes, todo: add to pandas df

--- Parser.py
+++ Parser.py
@@ -1,4 +1,4 @@
-from typing import List
+import bs4
 from bs4 import BeautifulSoup
 import requests 
 import numpy as np 
@@ -30,15 +30,21 @@ function: ParseHTML
                 Baths
                     Add all of these to df 
     return df 
+
+funtion: ParseCard
+    -----------------------
+    input: "li" html contents 
+    output: "div" attrs: "data-testid", class_ = "Card__CardWrapper-x1sjdn-0 gnintI"
+    -----------------------
 """
 
 class Parser: 
     main_url = "https://www.daft.ie/property-for-sale/dublin-city?pageSize=20&from="
 
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
-    def getHTMLResults(num_results=2000) -> List: 
+    def getHTMLResults(num_results: int=2000) -> list: 
         url_number_attribute = np.linspace(start=0,
                                             stop=num_results,
                                             num=int((num_results)/20))
@@ -51,10 +57,39 @@ class Parser:
                 .find_all("li", class_="SearchPage__Result-gg133s-2 itNYNv")
             results.extend(currResult)
         print(len(results))
-        return None;
+        return results
 
-    def ParseHTML() -> pd.DataFrame:
+    def parseHTML(htmlList: list) -> pd.DataFrame:
+        df = pd.DataFrame(columns=["Price", "Address", "Bedrooms", "Baths"])
+        for li_html in htmlList:
+            cardTitleBlock = Parser.parseDoc(li_html)
+            Parser.getAttributes(cardTitleBlock)
+            break
         pass 
 
+    def parseDoc(li_html: bs4.element.Tag):
+        soup = li_html 
+        #need to get cardwrapper;
+        cardWrapper = soup.find(attrs={"data-testid":"card-wrapper"}, class_="Card__CardWrapper-x1sjdn-0 gnintI")\
+            .find("div",attrs={"data-testid":"card-content"} ,class_="Card__Content-x1sjdn-9 iEbIAZ")\
+                .find("div", attrs={"data-testid":"title-block"})
+        return cardWrapper
 
-Parser.getHTMLResults()
+    def getAttributes(div_html: bs4.element.Tag):
+        soup = div_html
+        cardPrice = soup.find("span", class_="TitleBlock__StyledSpan-sc-1avkvav-4 gDBFnc").get_text()
+        cardAddress = soup.find("p", attrs={"data-testid":"address"}, class_="TitleBlock__Address-sc-1avkvav-7 knPImU").get_text()
+        cardBedrooms = soup.find("p", attrs={"data-testid":"beds"}, class_="TitleBlock__CardInfoItem-sc-1avkvav-8 jBZmlN").get_text()
+        cardBaths = soup.find("p", attrs={"data-testid":"baths"}, class_="TitleBlock__CardInfoItem-sc-1avkvav-8 jBZmlN").get_text()
+        cardArea = soup.find("p", attrs={"data-testid":"floor-area"}, class_="TitleBlock__CardInfoItem-sc-1avkvav-8 jBZmlN").get_text()
+        cardResults = [cardPrice, cardAddress, cardBedrooms, cardBaths, cardArea]
+        return cardResults
+
+
+def main():
+    HTMLList = Parser.getHTMLResults(100);
+    Parser.parseHTML(HTMLList)
+    return 0;
+
+if __name__ == '__main__':
+    main()
